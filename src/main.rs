abd042f35d5b6b0ac3d7705883efef009c466969
yanai0165 <yanai0165@gmail.com>
2022-10-17 20:04:37 +0300

    basic version of vga

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,12 @@
 #![no_main] 
 
 use core::panic::PanicInfo;
-
-// first commit
+mod vga_buffer;
 
 #[no_mangle] // don't give a random unique name for `_start` function
 pub extern "C" fn _start() -> ! {
-    
+    vga_buffer::print_something();
+
     loop {}
 }
 
@@ -15,3 +15,4 @@ pub extern "C" fn _start() -> ! {
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
+
