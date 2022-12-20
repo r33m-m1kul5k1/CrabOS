e12edc921fcb37bb4a422072a7ef49e285b13c07
yanai0165 <yanai0165@gmail.com>
2022-12-20 10:58:58 +0200

    added IT

--- tests/vmm.rs
+++ tests/vmm.rs
@@ -4,34 +4,22 @@
 #![test_runner(CrabOS::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-use CrabOS::{test_should_panic_handler, vmm};
+use bootloader::BootInfo;
+use x86_64::VirtAddr;
 use core::panic::PanicInfo;
-use x86_64::{
-    structures::paging::{
-        FrameAllocator, Mapper, OffsetPageTable, Page, PageTable, PhysFrame, Size4KiB,
-    },
-    PhysAddr, VirtAddr,
-};
+use CrabOS::{test_should_panic_handler, vmm};
 
 #[no_mangle]
-pub extern "C" fn _start() -> ! {
-    let mut mapper = <dyn Mapper<S>>::new();
-    vmm::init(mapper.physical_memory_offset());
+pub extern "C" fn _start(boot_info: &'static BootInfo) -> ! {
+    let virtual_add = VirtAddr { 
+        addr: boot_info.physical_memory_offset 
+    };
+    unsafe { vmm::init(virtual_add) };
     test_main();
     loop {}
 }
 
-
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     test_should_panic_handler(info)
-}
-
-#[test_case]
-fn basic_print() {
-    #[allow(unconditional_recursion)]
-    fn stack_overflow() {
-        stack_overflow()
-    }
-    stack_overflow();
 }
