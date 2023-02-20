0b9c1f3bbc73aa4fb7b6f36eb1b8f71cae7f7b84
YanaiZehavi <93211109+YanaiZehavi@users.noreply.github.com>
2023-02-20 18:55:51 +0200

    added thread object

--- src/processes/objects.rs
+++ src/processes/objects.rs
@@ -1,1 +1,28 @@
 //! this module defines thread and object structs
+
+pub struct Thread {
+    /// program counter
+    pub rip: u64,
+    pub cs: u64,
+    pub rflags: u64,
+    /// stack pointer  
+    pub rsp: u64,
+    pub ss: u64,
+
+    // registers
+    pub rax: u64,
+    pub rbx: u64,
+    pub rcx: u64,
+    pub rdx: u64,
+    pub rsi: u64,
+    pub rdi: u64,
+    pub r8w: u64,
+    pub r9w: u64,
+    pub r10w: u64,
+    pub r11w: u64,
+    pub r12w: u64,
+    pub r13w: u64,
+    pub r14w: u64,
+    pub r15w: u64,
+    
+}
